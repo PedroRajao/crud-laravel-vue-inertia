dede8cb1294e5f786542db24bd53186181d86d1f
s0608707 <pedro-marques@prodesan.com.br>
2024-08-29 09:38:15 -0300

    CRUD - Consulta e mostra registros na página

---
+++ resources/js/Pages/index.vue
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<template>
+    <div class="container mx-auto pt-5">
+      <!-- Loop pelos posts -->
+      <div class="post_block" v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.author }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  defineProps({
+    posts: Array // Definimos que "posts" será uma lista/array de objetos
+  });
+  </script>
+  
+  <style scoped>
+  /* Exemplo de estilos */
+  .post_block {
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+  
+  h2 {
+    font-size: 1.5em;
+  }
+  
+  p {
+    color: #555;
+  }
+  </style>
+  
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::get('/', [PostController::class, 'index']);
+
+
+// Route::get('/', function () {
+//     return Inertia::render('Home');
+// });
 
 Route::get('/about', function () {
     return Inertia::render('About');
